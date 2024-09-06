0a9d90cde1ec122b4e40d7faca286e53d9103bc3
SATLOCKLATAM\andres.londono <andres.londono@satlock.com>
2024-09-06 09:25:06 -0500

    creacion de la funcionalidad del .env

--- db/connection.go
+++ db/connection.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type DB struct {
@@ -13,7 +16,19 @@ type DB struct {
 var DbGlobalUltra2 *DB
 
 func ContectionDB() {
-	connStr := "user=postgres password=1000254878 host=localhost port=5432 dbname=co_crud sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error al cargar el archivo .env: %v", err)
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err.Error())
